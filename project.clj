f9dac231ea092b6556b2f3fdd0e4c6bad4862da2
Daniel E. Renfer <duck@kronkltd.net>
2012-12-22 12:49:56 -0500

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://github.com/duck1123/ciste"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :min-lein-version "2.0.0"
-  :dependencies [[ciste/ciste-core "0.4.0-SNAPSHOT"]
-                 [slacker "0.8.2"]]
+  :dependencies [[ciste "0.4.0-SNAPSHOT"]
+                 [slacker "0.8.5"]]
   :profiles {:dev
              {:dependencies
-              [[log4j "1.2.16"]
-               [midje "1.4.0"]]}}
-  :plugins [[codox "0.6.1"]
-            [lein-midje "2.0.0-SNAPSHOT"]]
+              [[log4j "1.2.17"]
+               [midje "1.5-alpha3"]]}}
+  :plugins [[codox "0.6.4"]
+            [lein-midje "2.0.3"]]
   :autodoc {:name "Ciste Service Slacker"
             :copyright "2012 KRONK Ltd."}
   :warn-on-reflection true
